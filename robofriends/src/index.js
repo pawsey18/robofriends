76923554413fbd249a8fee85f04a2ac37c61045a
Nick Pawsey <pawsey18@gmail.com>
2021-07-08 09:08:40 -0300

    ;?

---
+++ robofriends/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import App from "./App";
+import "tachyons";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
